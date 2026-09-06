601a9d15b7013ed1fddd740e4723cec980070ca3
free3et <free3et@gmail.com>
2026-09-06 17:22:08 +0300

    Add Trie data structure implementation with methods for inserting keys, retrieving keys by prefix, and collecting all keys

---
+++ design-of-algorithms/goit-algo2-hw-04/trie.py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.value = None
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.size = 0
+
+    def put(self, key, value=None):
+        if not isinstance(key, str) or not key:
+            raise TypeError(f"Illegal argument for put: key = {key} must be a non-empty string")
+
+        current = self.root
+        for char in key:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        if current.value is None:
+            self.size += 1
+        current.value = value
+
+    def keys_with_prefix(self, prefix):
+        if not isinstance(prefix, str):
+            raise TypeError(f"Illegal argument for keysWithPrefix: prefix = {prefix} must be a string")
+
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return []
+            current = current.children[char]
+
+        result = []
+        self._collect(current, list(prefix), result)
+        return result
+
+    def _collect(self, node, path, result):
+        if node.value is not None:
+            result.append("".join(path))
+        for char, next_node in node.children.items():
+            path.append(char)
+            self._collect(next_node, path, result)
+            path.pop()
+
+    def keys(self):
+        result = []
+        self._collect(self.root, [], result)
+        return result
